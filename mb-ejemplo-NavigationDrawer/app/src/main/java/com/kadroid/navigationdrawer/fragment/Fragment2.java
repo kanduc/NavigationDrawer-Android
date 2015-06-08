f1bdb686681c08cf770932d2dc9745a1fb6f1a9a
Kanduc <katluvh@gmail.com>
2015-06-07 23:34:38 -0500

    Ejemplo básico de navigation drawer en Android

---
+++ mb-ejemplo-NavigationDrawer/app/src/main/java/com/kadroid/navigationdrawer/fragment/Fragment2.java
@@ -0,0 +1,22 @@
+package com.kadroid.navigationdrawer.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kadroid.mb_ejemplo_navigationdrawer.R;
+
+/**
+ * Created by Kalu on 07/06/2015.
+ */
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
